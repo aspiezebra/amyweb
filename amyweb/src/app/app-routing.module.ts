0723f2592387fb1cead0b344a0a4fc53463c7edf
amy hufker <amyelizabethrosehufker@gmail.com>
2021-08-18 16:26:44 -0500

    add license

--- amyweb/src/app/app-routing.module.ts
+++ amyweb/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EhlersDanlosComponent } from './ehlers-danlos/ehlers-danlos.component';
+import { AutismComponent } from './autism/autism.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ' ', component: EhlersDanlosComponent },
+  { path: ' ', component: AutismComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
